bda7638ef32f6d5c393f14d793ca6e79a5aa7e5b
iamspruce <iamspruce8@gmail.com>
2021-03-11 14:56:43 +0100

    added

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: "xarMOMUI3aEMJddijF1td8HUJ",
+    consumer_secret: "MpzaJqvbSKilG3jMUksP9iIYr30o8RuRWSD2DnLlEeFBXYKk6A",
+    access_token: "1367424315520667648-SIqFFC7ZRDgP9amfHY0EoJWNac3qwd",
+    access_token_secret: "J4YSr5ii07Zt3yOdvLvz5zpRRABFi0gszUt8AGv3wDmGk"
+}
